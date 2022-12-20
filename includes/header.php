90e46fbbfbf9171bfdd862f0259355fda02183df
unknown <timafra2@mallon.com.br>
2022-12-20 17:55:43 -0300

    add files

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+
+    <!-- CSS Style -->
+    <link rel="stylesheet" href="../src/style/header.css">
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/1700f8d6e5.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+    <div id="header">
+        <a href="#">
+            <i class="fa-solid fa-cash-register fa-lg"> My Store</i>
+        </a>
+    </div>
+
+    <div id="container">
+        Conteudo
+    
+    
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+//includes
+require __DIR__ . '/includes/header.php';
+require __DIR__ . '/includes/footer.php';
+
+?>
