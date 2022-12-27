fdc786c793fc4e93651fa8d8f6020681a2659377
unknown <luccasdrapala@gmail.com>
2022-12-27 20:55:10 -0300

    construindo conexao do banco de dados

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Header</title>
+    <title>My Store</title>
 
     <!-- CSS Style -->
     <link rel="stylesheet" href="../src/style/header.css">
@@ -33,6 +33,4 @@
         </div>
     </header>
 
-    <div id="container">
-    conteudo
     
